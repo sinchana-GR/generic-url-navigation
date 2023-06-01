2d587730087d1703c9be9899b74630f0ee805512
sinchana-GR <sinchanagr2@gmail.com>
2023-06-01 22:55:07 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+
+# Create your views here.
+def bhavya(request):
+    return render(request,'bhavya.html')
+def akt(request):
+    return render(request,'akt.html')
